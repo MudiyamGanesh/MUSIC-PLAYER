16cecc4cb5521d2e2c1bd2a528ca1ba87bb60320
Ganesh Mudiyam <ramganeshm190@gmail.com>
2024-05-26 10:00:09 +0530

    Update mp.js

--- mp.js
+++ mp.js
@@ -11,7 +11,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ["Ney Veyrey", "Adhento Gaani","Naan Gaali","Enna Sona", "Neela yevaru", "Sajni", "Ye Mantramo"];
+const songs = ["Ney Veyrey", "Adhento Gaani","Naan Gaali","Enna Sona", "Neela yevaru", "Sajni", "Ye Mantramo", "Cheliya Cheliya"];
 
 // Keep track of song
 let songIndex = 4;
